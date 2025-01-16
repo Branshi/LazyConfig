ecf2bbac9cc1a53f94de50c525cec23843c2e05a
Trillyon Earl <trillyonearl@gmail.com>
2025-01-15 20:13:20 -0600

    Fixed Config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,5 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -12,16 +11,12 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  {
+    "xiyaowong/transparent.nvim"
+  },
   -- add transparent tokyonight
   {
     "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
   },
   -- Configure LazyVim to load tokyonight
   {
